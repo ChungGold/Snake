a793a7293a7e1fe73c07ae48b73696a550487bc6
ChungGold <chungau@csu.fullerton.edu>
2021-03-07 21:21:03 -0800

    added feature to stop users when they cross the game borders

--- index.js
+++ index.js
@@ -4,47 +4,56 @@ let y = 0;
 
 let x = 0;
 
-if (y > 14 
-|| x >= 30
-|| y < -14
-|| x <= -30){
-    alert('stop');
-}
-
 window.addEventListener("keydown", function(event){
     console.log(event.key);
     switch (event.key){
         case 'w':
         case 'ArrowUp':
-                setInterval(function(){
-                    y++;
-                    character.style.bottom = y + 'px';
-                }, 10)
-                console.log(x, y);
+            let up = setInterval(function(){
+                y++;
+                character.style.bottom = y + 'px';
+                if (y > 225) {
+                    clearInterval(up);
+                    alert('OUT OF ZONE');
+                }
+            }, 10)
+            console.log(x, y);
             break;
         case 'a':
         case 'ArrowLeft':
-                setInterval(function(){
-                    x--;
-                    character.style.left = x + 'px';
-                }, 10)
-                console.log(x, y);
+            let left = setInterval(function(){
+                x--;
+                character.style.left = x + 'px';
+                if (x < -470) {
+                    clearInterval(left);
+                    alert('OUT OF ZONE');
+                }
+            }, 10)
+            console.log(x, y);
             break;
         case 's':
         case 'ArrowDown':
-            setInterval(function(){
+            let down = setInterval(function(){
                 y--;
                 character.style.bottom = y + 'px';
+                if (y < -225) {
+                    clearInterval(down);
+                    alert('OUT OF ZONE');
+                }
             }, 10)
-                console.log(x, y);
+            console.log(x, y);
             break;
         case 'd':
         case 'ArrowRight':
-            setInterval(function(){
+            let right = setInterval(function(){
                 x++;
                 character.style.left = x + 'px';
+                if (x > 470) {
+                    clearInterval(right);
+                    alert('OUT OF ZONE');
+                }
             }, 10)
-                console.log(x, y);
+            console.log(x, y);
     }
 });
 
